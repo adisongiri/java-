db7ef30494f590f59cd6d594c0f0f53306b1443d
adisongiri <91259193+adisongiri@users.noreply.github.com>
2023-02-13 20:40:56 +0545

    hw
    hw

---
+++ adisoon.java
@@ -0,0 +1,19 @@
+public class adisoon
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Name of organization:chiya ghar");
+        System.out.println("   ");
+        System.out.println("Address:");
+        System.out.println("----State:kathmandu");
+        System.out.println("----Zipcode:46000");
+        System.out.println("----Country:Nepal");
+        System.out.println("   ");
+        System.out.println("Contact Information:");
+        System.out.println("----Mobile Number:987456123");
+        System.out.println("Email:kistcollege@edu.np");
+
+        
+        
+    }
+}
